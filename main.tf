68c0f555f051ce25423afad280b64ebb99dde9cc
sheetal-chandoskar <87699718+sheetal-chandoskar@users.noreply.github.com>
2021-12-01 22:12:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/searce/s3-webapp/aws"
+  source  = "xxxx"
   name        = var.name
   region = var.region
   prefix = var.prefix
